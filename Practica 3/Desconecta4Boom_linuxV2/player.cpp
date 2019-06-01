3f61e9d980b666ee74c5843d3e1f2a64983af74c
9pepe7 <pepenc97@hotmail.com>
2019-06-01 22:54:02 +0200

    Empiezo. alfa-beta

--- Practica 3/Desconecta4Boom_linuxV2/player.cpp
+++ Practica 3/Desconecta4Boom_linuxV2/player.cpp
@@ -53,11 +53,35 @@ double ValoracionTest(const Environment &estado, int jugador){
 
 // ------------------- Los tres metodos anteriores no se pueden modificar
 
-
+int Hor(const Environment &estado, int jug){
+return 0;
+}
+int Ver(const Environment &estado, int jug){
+return 0;
+}
+int Dia(const Environment &estado, int jug){
+return 0;
+}
 
 // Funcion heuristica (ESTA ES LA QUE TENEIS QUE MODIFICAR)
+// La primera parte es calcada a la test, la segunda es la que aplica nuestra heuristica
+// Se usan 3 funciones auxiliares para ver el estado del tablero
 double Valoracion(const Environment &estado, int jugador){
-}
+  int ganador = estado.RevisarTablero();
+
+  if (ganador==jugador)
+    return 99999999.0; // Gana el jugador que pide la valoracion
+  else if (ganador!=0)
+    return -99999999.0; // Pierde el jugador que pide la valoracion
+  else if (estado.Get_Casillas_Libres()==0)
+    return 0;  // Hay un empate global y se ha rellenado completamente el tablero
+  else { // Aplicamos la heuristica
+    int rival = (jugador==1)?2:1;
+    int yo_hor = Hor(estado, jugador),  yo_ver = Ver(estado, jugador),  yo_dia = Dia(estado, jugador),
+    rival_hor  = Hor(estado, rival), rival_ver = Ver(estado, rival), rival_dia = Dia(estado, rival);
+    return ((rival_hor+rival_ver+rival_dia)-(yo_hor+yo_ver+yo_dia));
+  } // Como las funciones suman en positivo las fichas seguidas, y nosotros buscamos que el rival las tenga,
+}   // la heuristica hace rival-yo, para favorecer aquellos estado en los que el rival enlace fichas
 
 
 
@@ -77,7 +101,43 @@ void JuegoAleatorio(bool aplicables[], int opciones[], int &j){
 }
 
 
+double Poda_AlfaBeta(const Environment &estado, int jug, bool min_max, int prof, Environment::ActionType &accion, double alpha, double beta){
+  if(prof==0 || estado.JuegoTerminado()) // Si termina el juego o la profundidad maxima se ha alcanzado (va bajando)
+    return Valoracion(estado, jug);
 
+  int u_accion = -1;
+  double valor;
+  Environment::ActionType ant_accion;
+  Environment hijo = estado.GenerateNextMove(u_accion);
+  bool VecAct[8];
+  int n_hijos = estado.possible_actions(VecAct); // Posibles futuras acciones
+
+  if(min_max){ // Si min_max es true, alpha
+    for(int i=0; i<n_hijos; ++i){
+      valor=Poda_AlfaBeta(hijo,jug,!min_max,prof-1,ant_accion,alpha,beta);
+      if(valor>alpha){
+        alpha=valor;
+        accion= static_cast<Environment::ActionType>(u_accion);
+      }
+      if(beta<=alpha) // malo
+        break;
+      hijo=estado.GenerateNextMove(u_accion);
+    }
+    return alpha;
+  } else{ // false, beta
+    for(int i=0; i<n_hijos; ++i){
+      valor=Poda_AlfaBeta(hijo,jug,!min_max,prof-1,ant_accion,alpha,beta);
+      if(valor<beta){
+        beta=valor;
+        accion= static_cast<Environment::ActionType>(u_accion);
+      }
+      if(beta<=alpha) // malo
+        break;
+      hijo=estado.GenerateNextMove(u_accion);
+    }
+    return beta;
+  }
+}
 
 
 
@@ -86,7 +146,7 @@ Environment::ActionType Player::Think(){
     const int PROFUNDIDAD_MINIMAX = 6;  // Umbral maximo de profundidad para el metodo MiniMax
     const int PROFUNDIDAD_ALFABETA = 8; // Umbral maximo de profundidad para la poda Alfa_Beta
 
-    Environment::ActionType accion; // acción que se va a devolver
+    Environment::ActionType accion; // acciï¿½n que se va a devolver
     bool aplicables[8]; // Vector bool usado para obtener las acciones que son aplicables en el estado actual. La interpretacion es
                         // aplicables[0]==true si PUT1 es aplicable
                         // aplicables[1]==true si PUT2 es aplicable
@@ -117,7 +177,7 @@ Environment::ActionType Player::Think(){
          cout << " " << actual_.ActionStr( static_cast< Environment::ActionType > (t)  );
     cout << endl;
 
-
+    /*
     //--------------------- COMENTAR Desde aqui
     cout << "\n\t";
     int n_opciones=0;
@@ -142,6 +202,7 @@ Environment::ActionType Player::Think(){
          }
 
     //--------------------- COMENTAR Hasta aqui
+    */
 
 
     //--------------------- AQUI EMPIEZA LA PARTE A REALIZAR POR EL ALUMNO ------------------------------------------------
@@ -149,9 +210,8 @@ Environment::ActionType Player::Think(){
 
     // Opcion: Poda AlfaBeta
     // NOTA: La parametrizacion es solo orientativa
-    // valor = Poda_AlfaBeta(actual_, jugador_, 0, PROFUNDIDAD_ALFABETA, accion, alpha, beta);
-    //cout << "Valor MiniMax: " << valor << "  Accion: " << actual_.ActionStr(accion) << endl;
-
+    alpha=menosinf; beta=masinf;
+    valor = Poda_AlfaBeta(actual_, jugador_, true, PROFUNDIDAD_ALFABETA, accion, alpha, beta);
+    cout << "Valor AlfaBeta: " << valor << "  Accion: " << actual_.ActionStr(accion) << endl;
     return accion;
 }
-
